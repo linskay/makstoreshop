909972ef953a82ec90269ac3604b7ba319375ff7
Lins <alina.cheremiskina@gmail.com>
2024-11-24 23:07:15 +0300

    master

---
+++ src/test/java/ru/shop/makstore/MakstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shop.makstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
