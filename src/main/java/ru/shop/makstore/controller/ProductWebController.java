d03f74db9b6ec69678182e301c70fcdd96e1f494
Lins <alina.cheremiskina@gmail.com>
2025-01-12 03:11:52 +0300

    sorry

--- src/main/java/ru/shop/makstore/controller/ProductWebController.java
+++ src/main/java/ru/shop/makstore/controller/ProductWebController.java
@@ -22,6 +22,6 @@ public class ProductWebController {
     public String getAllProducts(Model model) {
         List<Product> products = productService.getAllProducts();
         model.addAttribute("products", products);
-        return "products";
+        return "product";
     }
 }
